73fcab72102e96a7299f1f0f35c04fc9b108ebbe
stepan <stepan.ivanov93@gmail.com>
2017-08-16 00:30:42 +0300

    modal

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,12 +1,15 @@
+
+ 
+
  $(document).ready(function(){
            $(".arrow").click(function () {
                        if ($(".channel__menu").is(":hidden")) {
 
-                               $(".channel__menu").show();
+                               $(".channel__menu").$(this).parent().show();
 
                        } else {
 
-                               $(".channel__menu").hide();
+                               $(".channel__menu").$(this).parent().show().hide();
 
                        }
  return false;
@@ -24,13 +27,30 @@ $(".arrow").click(function(e) {
 
 
 
-$(".package__main-btn").click(function(e) {
-  e.preventDefault();
+
+// изменение кнопки "выбрать"
+function showDetail1(){
+   var $this=$(this); 
+    var $parent=$this.closest('.container');
+    var $detailBox=$parent.find('.channel__menu')
+}
+
+function selectBox1(){
+  var $this=$(this);  
+  var $parent=$this.closest('.container');
+  $('.package__container--selected').removeClass('package__container--selected');
   $(".package__main-btn").removeClass('package__main-btn--selected');
-  $(this).addClass('package__main-btn--selected');
+  $this.addClass('package__main-btn--selected');
+  $parent.addClass('package__container--selected')
+}
 
-})
+function ready(){
+  $('.arrow-container').on('click',showDetail1)
+  $('.package__main-btn').on('click',selectBox1)
+}  
+$(document).ready(ready)
 
+// изменение кнопки "добавить"
 function showDetail(){
    var $this=$(this); 
     var $parent=$this.closest('.container');
@@ -41,14 +61,14 @@ function selectBox(){
   var $this=$(this);  
   var $parent=$this.closest('.container');
   $('.package__container--selected').removeClass('package__container--selected');
-  $(".package__main-btn").removeClass('package__main-btn--selected');
-  $this.addClass('package__main-btn--selected');
+  $(".package__add-btn").removeClass('package__add-btn--selected');
+  $this.addClass('package__add-btn--selected');
   $parent.addClass('package__container--selected')
 }
 
 function ready(){
   $('.arrow-container').on('click',showDetail)
-  $('.package__main-btn').on('click',selectBox)
+  $('.package__add-btn').on('click',selectBox)
 }  
 $(document).ready(ready)
 
@@ -56,14 +76,32 @@ $(document).ready(ready)
 
 
 
+// mobile menu -----
+
 $(document).ready(function() {
   $('.menu-trigger').click(function() {
-    $('nav ul').slideToggle(500);
+    $('.nav__list').slideToggle(500);
   });//end slide toggle
   
   $(window).resize(function() {   
-    if (  $(window).width() > 500 ) {     
-      $('nav ul').removeAttr('style');
+    if (  $(window).width() > 1024 ) {     
+      $('.nav__list').removeAttr('style');
      }
   });//end resize
 });//end ready
+
+
+$(document).ready(function() {
+  $('.menu-trigger-phone').click(function() {
+    $('.nav__contact-list').slideToggle();
+  });//end slide toggle
+  
+  $(window).resize(function() {   
+    if (  $(window).width() > 1024 ) {     
+      $('.nav__contact-list').removeAttr('style');
+     }
+  });//end resize
+});//end ready
+
+
+
