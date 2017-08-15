ae0061ff78c670a2e2ab559559d9e349e5838f8a
Ivanov Stepan <stepan.ivanov93@gmail.com>
2017-08-15 14:28:46 +0300

    start mobile

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -51,3 +51,19 @@ function ready(){
   $('.package__main-btn').on('click',selectBox)
 }  
 $(document).ready(ready)
+
+
+
+
+
+$(document).ready(function() {
+  $('.menu-trigger').click(function() {
+    $('nav ul').slideToggle(500);
+  });//end slide toggle
+  
+  $(window).resize(function() {   
+    if (  $(window).width() > 500 ) {     
+      $('nav ul').removeAttr('style');
+     }
+  });//end resize
+});//end ready
